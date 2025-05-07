1bd0b811f873c137af8e1127d89709d4e93a5409
Ricardo Antonio Gómez Sotomayor <ricardo.gomez@tecsup.edu.pe>
2025-05-07 14:02:11 -0500

    Agregado frontend básico con HTML, CSS y JS

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-require('dotenv').config()
-const http= require('http')
+require('dotenv').config();
+const http = require('http');
+const express = require('express');
 
-function requestController() {
-    console.log('Bienvenidos al curso')
-}
+// Inicializar Express
+const app = express();
 
-const server= http.createServer(requestController)
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static('public'));
 
-const PORT= process.env.PORT
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
-server.listen(PORT, function(){
-    console.log("Aplicacion corriendo en: " + PORT)
-})
+// Crear el servidor
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, function() {
+  console.log("Aplicación corriendo en: " + PORT);
+});
